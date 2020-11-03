e8e3054725a8c653b41b39aa77a6c78e55e5f030
Stanley <lims95@yahoo.com>
2020-11-03 15:19:17 -0500

    seed

--- tests/models.spec.js
+++ tests/models.spec.js
@@ -7,13 +7,23 @@ describe("Models relationships", () => {
     const lastName = faker.name.lastName();
     const email = faker.internet.email();
     const password = faker.internet.password();
-    const user = await User.create({
+    const user1 = await User.create({
       firstName,
       lastName,
       email,
       password,
       admin: true,
     });
-    expect(user.firstName).toEqual(firstName);
+    expect(user1.firstName).toEqual(firstName);
+    expect(user1.lastName).toEqual(lastName);
+    expect(user1.email).toEqual(email);
+    expect(user1.password).toEqual(password);
+
+    const projectName = faker.lorem.word();
+    const projectDescription = faker.lorem.sentence();
+    const project1 = await Project.create({
+      title: projectName,
+      description: projectDescription,
+    });
   });
 });

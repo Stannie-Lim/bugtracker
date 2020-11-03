b30275cf6b0fcc2569b2f0fd57ad7db0659b6ce6
Stanley <lims95@yahoo.com>
2020-11-03 15:26:33 -0500

    added tests

--- tests/models.spec.js
+++ tests/models.spec.js
@@ -2,11 +2,11 @@ const { User, Project, Ticket } = require("../src/server/db/models");
 const faker = require("faker");
 
 describe("Models relationships", () => {
+  const firstName = faker.name.firstName();
+  const lastName = faker.name.lastName();
+  const email = faker.internet.email();
+  const password = faker.internet.password();
   it("Successfully creates models", async () => {
-    const firstName = faker.name.firstName();
-    const lastName = faker.name.lastName();
-    const email = faker.internet.email();
-    const password = faker.internet.password();
     const user1 = await User.create({
       firstName,
       lastName,
@@ -20,15 +20,18 @@ describe("Models relationships", () => {
     expect(user1.password).toEqual(password);
   });
 
-  // it('Successfully adds project to user', async() => {
-  //   const projectName = faker.lorem.word();
-  //   const projectDescription = faker.lorem.sentence();
-  //   const project1 = await Project.create({
-  //     title: projectName,
-  //     description: projectDescription
-  //   });
+  it("Successfully adds project to user", async () => {
+    const projectName = faker.lorem.word();
+    const projectDescription = faker.lorem.sentence();
+    const project1 = await Project.create({
+      title: projectName,
+      description: projectDescription,
+    });
 
-  //   await user1.addProject(project1);
-  //   expect(project1.adminId === user1.id);
-  // });
+    const user1 = await User.findOne({
+      where: { firstName, lastName, email, password },
+    });
+    await user1.addProject(project1);
+    expect(project1.adminId === user1.id);
+  });
 });

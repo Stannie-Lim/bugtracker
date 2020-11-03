d4f256d3aa008612a18e26ea22c5f91be5367926
Stanley <lims95@yahoo.com>
2020-11-03 17:05:11 -0500

    project detail

--- src/client/components/dashboard/ProjectDetail.js
+++ src/client/components/dashboard/ProjectDetail.js
@@ -1,13 +1,20 @@
 import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 const ProjectDetail = ({ match }) => {
-  useEffect(() => {
-    const url = match.params.projectId;
-    console.log(url);
-  }, []);
+  const projectId = match.params.projectId;
+  const project = useSelector(({ projects }) =>
+    projects.find((project) => project.id === projectId)
+  );
+  const tickets = useSelector(({ tickets }) =>
+    tickets.filter((ticket) => ticket.projectId === projectId)
+  );
+
+  console.log(tickets);
   return (
     <div className="main">
-      <h1>HI</h1>
+      <h1>{project.title}</h1>
+      <h3>{project.description}</h3>
     </div>
   );
 };

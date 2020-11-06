58d83f1c4380eb0818430207f362e34824ff4b9f
Stanley <lims95@yahoo.com>
2020-11-05 23:07:06 -0500

    createproject form

--- src/client/components/dashboard/AddProject.js
+++ src/client/components/dashboard/AddProject.js
@@ -4,7 +4,11 @@ import { useDispatch } from "react-redux";
 // store
 import { createProject } from "../../store/store";
 
+// css
+import "./addProject.css";
+
 // materialui
+import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -21,25 +25,39 @@ const AddProject = ({ history }) => {
 
   const classes = useStyles();
   return (
-    <div className="main">
+    <div className="create-project-form">
       <form onSubmit={addProject}>
-        <TextField
-          label="Title"
-          id="outlined-size-normal"
-          variant="outlined"
-          value={title}
-          onChange={({ target }) => setTitle(target.value)}
-          required={true}
-        />
-        <TextField
-          label="Description"
-          id="outlined-size-normal"
-          variant="outlined"
-          value={description}
-          onChange={({ target }) => setDescription(target.value)}
-          required={true}
-        />
-        <button>Create a new project</button>
+        <div>
+          <TextField
+            label="Title"
+            id="outlined-size-normal"
+            variant="outlined"
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+            required={true}
+            margin="dense"
+            className="create-project-text-field"
+          />
+        </div>
+        <div>
+          <TextField
+            label="Description"
+            id="filled-multiline-static"
+            multiline
+            rows={5}
+            variant="outlined"
+            value={description}
+            onChange={({ target }) => setDescription(target.value)}
+            required={true}
+            margin="dense"
+            className="create-project-text-field"
+          />
+        </div>
+        <div className="create-project-button">
+          <Button variant="contained" color="primary">
+            Create a new project
+          </Button>
+        </div>
       </form>
     </div>
   );

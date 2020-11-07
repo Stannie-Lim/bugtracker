c2912766176a5286f892d2ab3950853816461eff
Stanley <lims95@yahoo.com>
2020-11-06 22:36:48 -0500

    typography

--- src/client/components/dashboard/projects/ProjectDetailHeader.js
+++ src/client/components/dashboard/projects/ProjectDetailHeader.js
@@ -1,10 +1,20 @@
 import React from "react";
+import Divider from "@material-ui/core/Divider";
+import Typography from "@material-ui/core/Typography";
+
+// css
+import "./ProjectDetailHeader.css";
 
 const ProjectDetailHeader = ({ project }) => {
   return (
-    <div>
-      <h1>{project.title}</h1>
-      <h3>{project.description}</h3>
+    <div className="project-detail-header">
+      <Typography variant="h1" component="h2">
+        {project.title}
+      </Typography>
+      <Typography variant="h4" gutterBottom>
+        {project.description}
+      </Typography>
+      <Divider />
     </div>
   );
 };

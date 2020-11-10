45499ab9b6a1c21c6f335acb425548d1d4c0b150
Stanley <lims95@yahoo.com>
2020-11-10 15:19:58 -0500

    priority tickets

--- src/client/components/dashboard/TicketsByPriority.js
+++ src/client/components/dashboard/TicketsByPriority.js
@@ -1,23 +1,50 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 // sort
 import sortByPriority from "./sorts/sortByPriority";
 
 // components
+import Back from "./Back";
 import TicketCard from "./cards/TicketCard";
+
+// css
+import "./TicketsByPriority.css";
 
 const TicketsByPriority = () => {
   const tickets = useSelector(({ tickets }) => sortByPriority(tickets));
   return (
     <div className="main">
-      <Link to="/home">Back</Link>
-      <div>
-        {tickets &&
-          tickets.map((ticket) => (
-            <TicketCard key={ticket.id} ticket={ticket} />
-          ))}
+      <Back />
+      <div className="columns">
+        <div>
+          <h1>No Priority</h1>
+          {tickets &&
+            tickets
+              .filter((ticket) => ticket.priority === "NONE")
+              .map((ticket) => <TicketCard key={ticket.id} ticket={ticket} />)}
+        </div>
+        <div>
+          <h1>Low Priority</h1>
+          {tickets &&
+            tickets
+              .filter((ticket) => ticket.priority === "LOW")
+              .map((ticket) => <TicketCard key={ticket.id} ticket={ticket} />)}
+        </div>
+        <div>
+          <h1>Medium Priority</h1>
+          {tickets &&
+            tickets
+              .filter((ticket) => ticket.priority === "MEDIUM")
+              .map((ticket) => <TicketCard key={ticket.id} ticket={ticket} />)}
+        </div>
+        <div>
+          <h1>High Priority</h1>
+          {tickets &&
+            tickets
+              .filter((ticket) => ticket.priority === "HIGH")
+              .map((ticket) => <TicketCard key={ticket.id} ticket={ticket} />)}
+        </div>
       </div>
     </div>
   );

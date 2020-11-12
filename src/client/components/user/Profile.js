15683ac7b40868f77cbd7e7e8218f55991506f41
Stanley <lims95@yahoo.com>
2020-11-12 17:56:51 -0500

    no image available

--- src/client/components/user/Profile.js
+++ src/client/components/user/Profile.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
+  const user = useSelector(({ user }) => user);
   return (
     <div className="main">
       <h1>profile</h1>
+      <img src={user.imageUrl} />
     </div>
   );
 };
--- src/server/db/models/User.js
+++ src/server/db/models/User.js
@@ -36,6 +36,11 @@ const User = db.define("user", {
       notEmpty: true,
     },
   },
+  imageUrl: {
+    type: STRING,
+    defaultValue:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/480px-No_image_available.svg.png",
+  },
   fullName: {
     type: VIRTUAL,
     get() {

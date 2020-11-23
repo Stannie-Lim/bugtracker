1e4b0768743cf99bf66eaab422a73cdc7f417bef
Stanley <lims95@yahoo.com>
2020-11-23 04:57:46 -0500

    db config

--- src/server/db/db.js
+++ src/server/db/db.js
@@ -2,31 +2,20 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const Sequelize = require("sequelize");
-
-const productionSequelize = new Sequelize(
+const db = new Sequelize(
   process.env.DB_SCHEMA || "postgres",
   process.env.DB_USER || "postgres",
   process.env.DB_PASSWORD || "",
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 5432,
     dialect: "postgres",
     dialectOptions: {
-      ssl: process.env.DB_SSL,
+      ssl: process.env.DB_SSL == "true",
     },
   }
+  // process.env.DATABASE_URL || "postgres://localhost/bug_tracker",
+  // { logging: false }
 );
-
-const developmentSequelize = new Sequelize(
-  process.env.DATABASE_URL || "postgres://localhost/bug_tracker",
-  { logging: false }
-);
-
-console.log(productionSequelize);
-
-const db =
-  process.NODE_ENV === "production"
-    ? productionSequelize
-    : developmentSequelize;
 
 module.exports = db;

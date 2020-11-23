cd2062a42a946cb7cced35b521ed31586aeafea0
Stanley <lims95@yahoo.com>
2020-11-23 04:57:07 -0500

    db

--- src/server/db/db.js
+++ src/server/db/db.js
@@ -8,8 +8,8 @@ const productionSequelize = new Sequelize(
   process.env.DB_USER || "postgres",
   process.env.DB_PASSWORD || "",
   {
-    host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || 5432,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "postgres",
     dialectOptions: {
       ssl: process.env.DB_SSL,

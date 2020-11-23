89e8e573f6fa5a6257eb4709539cbe0cb5f34b88
Stanley <lims95@yahoo.com>
2020-11-23 04:47:21 -0500

    fixed db

--- src/server/db/db.js
+++ src/server/db/db.js
@@ -1,22 +1,23 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+console.log(process.env.NODE_ENV);
+
 const Sequelize = require("sequelize");
 const db = new Sequelize(
-  process.env.NODE_ENV !== "development"
-    ? (process.env.DB_SCHEMA || "postgres",
-      process.env.DB_USER || "postgres",
-      process.env.DB_PASSWORD || "",
-      {
-        host: process.env.DB_HOST || "localhost",
-        port: process.env.DB_PORT || 5432,
-        dialect: "postgres",
-        dialectOptions: {
-          ssl: process.env.DB_SSL == "true",
-        },
-      })
-    : (process.env.DATABASE_URL || "postgres://localhost/bug_tracker",
-      { dialect: "postgres", logging: false })
+  process.env.DB_SCHEMA || "postgres",
+  process.env.DB_USER || "postgres",
+  process.env.DB_PASSWORD || "",
+  {
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: process.env.DB_SSL == "true",
+    },
+  }
+  // process.env.DATABASE_URL || "postgres://localhost/bug_tracker",
+  // { logging: false }
 );
 
 module.exports = db;

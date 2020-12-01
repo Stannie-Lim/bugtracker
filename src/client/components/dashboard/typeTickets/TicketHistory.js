68f89ee210bf838166e2eff4241743d4b194c2c1
Stanley <lims95@yahoo.com>
2020-12-01 16:25:16 -0500

    almost done with tickets by type

--- src/client/components/dashboard/typeTickets/TicketHistory.js
+++ src/client/components/dashboard/typeTickets/TicketHistory.js
@@ -20,30 +20,71 @@ import ListItemText from "@material-ui/core/ListItemText";
 const TicketHistory = ({ tickets, ticket }) => {
   const classes = useStyles();
   const [historyOpen, setHistoryOpen] = useState(false);
+  const [ticketVisibilityOpen, setTicketVisibilityOpen] = useState(false);
+  const [controlsOpen, setControlsOpen] = useState(false);
 
   const handleHistoryClick = () => {
     setHistoryOpen(!historyOpen);
   };
 
+  const handleVisibilityOpen = () => {
+    setTicketVisibilityOpen(!ticketVisibilityOpen);
+  };
+
+  const handleControlsOpen = () => {
+    setControlsOpen(!controlsOpen);
+  };
+
   return (
     <div>
-      <ListItem button onClick={handleHistoryClick} className={classes.nested}>
+      <ListItem
+        button
+        onClick={handleVisibilityOpen}
+        className={classes.nested}
+      >
         <ListItemIcon>
           <InboxIcon />
         </ListItemIcon>
         <ListItemText primary={ticket.info} />
-        {historyOpen ? <ExpandLess /> : <ExpandMore />}
+        {ticketVisibilityOpen ? <ExpandLess /> : <ExpandMore />}
       </ListItem>
 
-      <Collapse in={historyOpen} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-          {tickets &&
-            tickets.map(({ tickethistories }) => {
-              return tickethistories.map((history) => (
-                <HistoryItem key={history.id} history={history} />
-              ));
-            })}
-        </List>
+      <Collapse in={ticketVisibilityOpen} timeout="auto" unmountOnExit>
+        <ListItem
+          button
+          onClick={handleControlsOpen}
+          className={classes.nested}
+        >
+          <ListItemText primary="Controls" />
+          {ticketVisibilityOpen ? <ExpandLess /> : <ExpandMore />}
+
+          <List component="div" disablePadding>
+            <Collapse in={controlsOpen} timeout="auto" unmountOnExit>
+              <List component="div" disablePadding>
+                <h1>Controls</h1>
+              </List>
+            </Collapse>
+          </List>
+        </ListItem>
+
+        <ListItem
+          button
+          onClick={handleHistoryClick}
+          className={classes.nested}
+        >
+          <ListItemText primary="History" />
+          {historyOpen ? <ExpandLess /> : <ExpandMore />}
+          <Collapse in={historyOpen} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              {tickets &&
+                tickets.map(({ tickethistories }) => {
+                  return tickethistories.map((history) => (
+                    <HistoryItem key={history.id} history={history} />
+                  ));
+                })}
+            </List>
+          </Collapse>
+        </ListItem>
       </Collapse>
     </div>
   );

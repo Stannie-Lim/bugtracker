b2258e647a8bf11c2d790842e4290f91fb1507fd
Stanley <lims95@yahoo.com>
2020-12-30 14:48:21 -0500

    register page

--- src/client/components/auth/Register.js
+++ src/client/components/auth/Register.js
@@ -8,12 +8,19 @@ import "./register.css";
 // store
 import { register } from "../../store/store";
 
+// materialui
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+
 const Register = () => {
   const [email, setEmail] = useState("");
   const [image, setImage] = useState(null);
   const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
   const [firstName, setFirstName] = useState("");
+  const classes = useStyles();
   const dispatch = useDispatch();
 
   const registerNewAccount = (ev) => {
@@ -41,47 +48,110 @@ const Register = () => {
   }
 
   return (
-    <div className="authform">
-      <form onSubmit={registerNewAccount}>
-        <input
-          type="text"
-          onChange={({ target }) => setFirstName(target.value)}
-          value={firstName}
-          placeholder="First Name"
-        />
-        <input
-          type="text"
-          onChange={({ target }) => setLastName(target.value)}
-          value={lastName}
-          placeholder="Last Name"
-        />
-        <input
-          type="text"
-          onChange={({ target }) => setEmail(target.value)}
-          value={email}
-          placeholder="Email"
-        />
-        <input
-          type="text"
-          onChange={({ target }) => setPassword(target.value)}
-          value={password}
-          placeholder="Password"
-        />
-        {/* <input
+    <div className="main-page">
+      <Typography variant="h4" gutterBottom>
+        Welcome to Bug Tracker
+      </Typography>
+      <div className="auth-form">
+        <form onSubmit={registerNewAccount}>
+          <div>
+            <TextField
+              required
+              id="outlined-required"
+              label="First Name"
+              variant="outlined"
+              onChange={({ target }) => setFirstName(target.value)}
+              value={firstName}
+              className="auth-text-field"
+              margin="dense"
+              inputProps={{ style: { fontSize: 20 } }}
+              autoFocus={true}
+            />
+          </div>
+          <div>
+            <TextField
+              required
+              id="outlined-required"
+              label="Last Name"
+              variant="outlined"
+              onChange={({ target }) => setLastName(target.value)}
+              value={lastName}
+              className="auth-text-field"
+              margin="dense"
+              inputProps={{ style: { fontSize: 20 } }}
+            />
+          </div>
+          <div>
+            <TextField
+              required
+              id="outlined-required"
+              label="Email"
+              variant="outlined"
+              onChange={({ target }) => setEmail(target.value)}
+              value={email}
+              className="auth-text-field"
+              margin="dense"
+              inputProps={{ style: { fontSize: 20 } }}
+            />
+          </div>
+          <div>
+            <TextField
+              required
+              id="outlined-password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              variant="outlined"
+              onChange={({ target }) => setPassword(target.value)}
+              value={password}
+              className="auth-text-field"
+              margin="dense"
+              inputProps={{ style: { fontSize: 20 } }}
+            />
+          </div>
+          <div className="authbuttons">
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit"
+              classes={{ root: classes.button }}
+            >
+              Register
+            </Button>
+          </div>
+          {/* <input
           type="file"
           name="image"
           onChange={({ target }) => setImage(target.files[0])}
           required
         /> */}
-        {image && <img src={window.URL.createObjectURL(image)} />}
-        <button>Register</button>
-        <h1>
-          Already have an account? <Link to="login">Click here</Link> to sign
-          in!
-        </h1>
-      </form>
+          {/* {image && <img src={window.URL.createObjectURL(image)} />} */}
+          <h1>
+            Already have an account? <Link to="login">Click here</Link> to sign
+            in!
+          </h1>
+        </form>
+      </div>
     </div>
   );
 };
 
 export default Register;
+
+// css
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: 200,
+    },
+  },
+  button: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    border: 0,
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+  },
+}));
